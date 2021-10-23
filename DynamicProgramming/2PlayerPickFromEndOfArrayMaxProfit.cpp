18b7e86844cf0583f9bc9d2149b20e9f0ee4d218
Pankaj Kumar <pankaj.54666@gmail.com>
2021-10-24 00:15:59 +0530

    GameProfit: updated

--- DynamicProgramming/2PlayerPickFromEndOfArrayMaxProfit.cpp
+++ DynamicProgramming/2PlayerPickFromEndOfArrayMaxProfit.cpp
@@ -3,39 +3,57 @@
 #include<vector>
 using namespace std;
 
+typedef long long int lli;
+const int NRow  = 1000 +10;
+const int NCol = NRow;
+int mem[NRow][NCol];
 
-int maxScore(int l, int r, const vector<int> &arr)
+lli maxScoreTwo(int l, int r, const vector<int> &arr)
 {
     if(l>r)
         return 0;
     if(l==r)
         return arr[l];
-    if(l+1 ==r )
-        return max(arr[l],arr[r]);
+    
+    if(mem[l][r] != -1)
+        return mem[l][r];
+
+    lli t[4];
+    t[0]  = maxScoreTwo(l+2,r,arr); //Maximum score of Player1 if Player2  chose l+1 (so our player 1 is left with array of [l+2,r])
+    t[1]  = maxScoreTwo(l+1,r-1,arr); //Maximum score of Player1 if Player2 choses r (so our player1 is left with array of [l+1,r-1])
+    lli lScore = arr[l] + min(t[0],t[1]); //    //Player1 pick left side + Choice Such that Player2 score is maximum.
+    /*
+    (Now after this game control will reach to player2, which he will PLAY TO WIN.
+    Hence, he will NOT LET player1 pick max. He will force player1 to pick MIN . 
+    So, that's why out of two scenerio. We need to add MIN only,as playe2 will force us to pick this(as he will maximize his profit by picking max for himself)
+    */
 
 
-    int t[4];
-    t[0]  = arr[l+1] +  maxScore(l+2,r,arr); //Player2 user chose l+1 (so our player 1 is left with array of [l+2,r])
-    t[1]  =arr[r] +  maxScore(l+1,r-1,arr); //Player2 choses r (so our player1 is left with array of [l+1,r-1])
-    int lScore = arr[l] + max(t[0],t[1]); //    //Player1 pick left side + Target is to minimize Player2 Score
+    t[2] =  maxScoreTwo(l,r-2,arr); //Maximum score of Player1 if Player2 also picks right element from remaining array
+    t[3] =  maxScoreTwo(l+1,r-1,arr); //Maximum score of Player1 if   Player2 pick rom left , from remainign array
 
+    lli rScore = arr[r] + min(t[2],t[3]); //  Player1 Pick right side element + Player2 force us to pick min amoung rest of element
 
-    t[2] = arr[r-1] + maxScore(l,r-2,arr); //Player2 also picks right element from remaining array
-    t[3] = arr[l] +  maxScore(l+1,r-1,arr); //Player1 pick rom left , from remainign array
-
-    int rScore = arr[r] + max(t[2],t[3]); //  Player1 Pick right side element + minimize Player2 score
-
-    return max(lScore,rScore);
+    int rVal = max(lScore,rScore);
+    mem[l][r] = rVal;
+    return rVal;
 }
 
 int main()
 {
+       for(int i=0;i<NRow;i++)
+        for(int j=0;j<NCol;j++)
+            mem[i][j] = -1;
+               for(int i=0;i<NRow;i++)
+        for(int j=0;j<NCol;j++)
+            mem[i][j] = -1;
    vector<int> arr{5,3,7,10};
-    int ans = maxScore(0,arr.size()-1,arr);
+    int ans = maxScoreTwo(0,arr.size()-1,arr);
     int tSum = 0;
     for(auto k:arr)
         tSum = tSum + k;
 
+ 
 
     printf("Player 1 MaX pOSSBILE SCORe = %d\n",ans);
     printf("Difference b/w score when both play optimal is = %d\n",tSum - ans);
