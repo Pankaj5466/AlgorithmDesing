18c6c7ac6a009760dbb3148da800f5da4f9e7513
Pankaj Kumar <pankaj.54666@gmail.com>
2021-11-24 20:34:03 +0530

    PairSumExactlyK

---
+++ ArrayProblems/SortingAlog/findElementInSortedArray.cpp
@@ -0,0 +1,34 @@
+//Problem Practice Link: 
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/*
+kakrafoon
+Read More
+Took me a while to sort out the cases. 
+I always get stuck figuring out whether to use '<=' or '<' in these type of problems. In this case, while the binary search itself was fine (<=), while looking for the pivot, 
+(a)I used '<'. Need to practice until this becomes second nature I think.
+(b) Either that or just fix (<=) as the convention and take it from there.
+*/
+
+int findPivote(int l, int r, const vector<int>&arr)
+{
+
+}
+
+int binarySearch(int l, int r, const vector<int>& arr)
+{
+    if(l>r)
+        return -1; //Elemetn do not exist
+}
+
+//one pass solution
+int modifiedBinarySearch(int l, int r, const vector<int> &arr)
+{
+
+}
+int main()
+{
+    return 0;
+}
